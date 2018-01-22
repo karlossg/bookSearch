9ebb50b15fa8302514748625af57aedf5f68d3fa
Karol <karol.gratus@gmail.com>
2018-01-22 15:17:42 +0100

    render books list works

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import api from '../utils/api';
 
-const Results = () => {
-  api
-    .getBooks('polski')
-    .then(resp => {
-      // resp.data.map(item => books.push(data));
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
 
-      return resp.data;
-    })
-    .then(function(resp) {
-      console.log(resp);
-    });
+  componentDidMount() {
+    api
+      .getBooks('polski')
+      .then(resp => {
+        // resp.data.map(item => books.push(data));
 
-  // <ul>{resp.data.map(item => <li>{item}</li>)}</ul>);
-  // getData() {
-  //   fetch('https://gwo.pl/booksApi/v1/search?query=historia')
-  //     .then(response => response.json())
-  //     .catch(error => console.error('Error:', error))
-  //     .then(data => console.log(data))
-  // }
-  // componentDidMount() {
-  //   this.getData();
-  // }
-};
+        return resp.data;
+      })
+      .then(
+        function(resp) {
+          this.setState(function() {
+            return {
+              books: resp
+            };
+          });
+        }.bind(this)
+      );
+  }
+
+  render() {
+    return <ul>{this.state.books.map(book => <li>{book.author}</li>)}</ul>;
+  }
+}
 
 export default Results;
